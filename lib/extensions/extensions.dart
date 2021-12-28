1c979303d423b7f13b843cafdd93fb1d236dfb93
raden-dimas012 <dimascode012@gmail.com>
2021-12-28 08:34:50 +0700

    fixing login

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,7 @@
+// @dart=2.9
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:bwa_flutix/models/models.dart';
+import 'package:bwa_flutix/services/services.dart';
+
+part 'firebase_user_extension.dart';
+
---
+++ lib/services/services.dart
@@ -0,0 +1,10 @@
+// @dart=2.9
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:bwa_flutix/extensions/extensions.dart';
+import 'package:bwa_flutix/models/models.dart';
+import 'package:bwa_flutix/shared/shared.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:http/http.dart' as http;
+
+part 'auth_services.dart';
+part 'user_services.dart';
