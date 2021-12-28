f97c515ee572d1ba5bbd6324620dea8eb30622eb
raden-dimas012 <dimascode012@gmail.com>
2021-12-28 21:27:42 +0700

    error still exist

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:bwa_flutix/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({ Key? key }) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
   // final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
 
   @override
