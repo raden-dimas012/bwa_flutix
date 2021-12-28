8434e3af3e7903ea55613b068ccfd5424c54e88f
raden-dimas012 <dimascode012@gmail.com>
2021-12-28 08:36:48 +0700

    fixing error

---
+++ lib/models/models.dart
@@ -0,0 +1,7 @@
+// @dart=2.9
+import 'dart:io';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+part 'user.dart';
---
+++ lib/services/user_services.dart
@@ -0,0 +1,30 @@
+// @dart=2.9
+part of 'services.dart';
+
+class UserServices {
+  static final CollectionReference _userCollection =
+      FirebaseFirestore.instance.collection('users');
+
+  static Future<void> updateUser(User user) async {
+    _userCollection.doc(user.id).set({
+      'email': user.email,
+      'name': user.name,
+      'balance': user.balance,
+      'selectedGenres': user.selectedGenres,
+      'selectedLanguage': user.selectedLanguage,
+      'profilePicture': user.profilePicture ?? ''
+    });
+  }
+
+  static Future<User> getUser(String id) async {
+    DocumentSnapshot snapshot = await _userCollection.doc(id).get();
+    return User(id, snapshot['email'],
+        balance: snapshot['balance'],
+        profilePicture: snapshot['profilePicture'],
+        selectedGenres: (snapshot['selectedGenres'] as List)
+            .map((e) => e.toString())
+            .toList(),
+        selectedLanguage: snapshot['selectedLanguage'],
+        name: snapshot['name']);
+  }
+}
