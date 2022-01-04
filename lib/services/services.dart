1fed9f2dca51d860d5e2fb8be27262e2079f0531
raden-dimas012 <dimascode012@gmail.com>
2022-01-04 08:40:08 +0700

    signup finishing and movie page

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:bwa_flutix/extensions/extensions.dart';
 import 'package:bwa_flutix/models/models.dart';
 import 'package:bwa_flutix/shared/shared.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
 part 'auth_services.dart';
 part 'user_services.dart';
+part 'movie_services.dart';
